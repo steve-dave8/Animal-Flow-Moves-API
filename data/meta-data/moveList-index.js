0f9805eb90dd8fb98b9bab6d3c09cd901e1ba519
Steven White <steven_green_white@hotmail.com>
2021-08-23 17:37:02 -0230

    Add more moves and combinations

--- data/meta-data/moveList-index.js
+++ data/meta-data/moveList-index.js
@@ -4,8 +4,10 @@ Moves with aliases are indented. Where necessary, precursors are noted in bracke
 >>>>>Static Activations:
 set static beast
 set static crab
+    to crab
     fall back to crab
 set deep ape
+    to ape
     push back to deep ape
 set loaded beast
     return to loaded beast
@@ -22,9 +24,12 @@ left leg scorpion reach (precursor: static beast)
 right leg scorpion reach (precursor: static beast)
     into right leg scorpion reach (precursor: static beast)
 left leg beast reach
+    into left leg beast reach
 right leg beast reach
+    into right leg beast reach
 ape reach
     return to ape reach
+    to ape reach
 open
 left arm crab reach
     into left arm crab reach
@@ -34,23 +39,27 @@ right arm crab reach
 return to crab (precursor: right arm crab reach)
 
 >>>>>Switches and Transitions
-LEVEL 1
+>LEVEL 1<
 left leg underswitch (precursor: static crab)
     into left leg underswitch (precursor: static crab)
 right leg underswitch (precursor: static crab)
     into right leg underswitch (precursor: static crab)
 left leg underswitch (precursor: static beast)
     slide to left leg underswitch (precursor: static beast)
+    pop back to left leg underswitch (precursor: static beast)
 right leg underswitch (precursor: static beast)
     slide to right leg underswitch (precursor: static beast)
+    pop back to right leg underswitch (precursor: static beast)
 left leg underswitch tap (precursor: static crab)
     into left leg underswitch tap (precursor: static crab)
 right leg underswitch tap (precursor: static crab)
     into right leg underswitch tap (precursor: static crab)
 left leg underswitch tap (precursor: static beast)
     slide to left leg underswitch tap (precursor: static beast)
+    pop back to left leg underswitch tap (precursor: static beast)
 right leg underswitch tap (precursor: static beast)
     slide to right leg underswitch tap (precursor: static beast)
+    pop back to right leg underswitch tap (precursor: static beast)
 left leg jumping underswitch
     into left leg jumping underswitch
 right leg jumping underswitch
@@ -58,32 +67,47 @@ right leg jumping underswitch
 left leg levitating underswitch
 right leg levitating underswitch
 left leg side kickthrough
+    to left leg side kickthrough
     slide to left leg side kickthrough
+    pop back to left leg side kickthrough
     jump to left leg side kickthrough
     levitate to left leg side kickthrough
 right leg side kickthrough
+    to right leg side kickthrough
     slide to right leg side kickthrough
+    pop back to right leg side kickthrough
     jump to right leg side kickthrough
     levitate to right leg side kickthrough
 left leg front step
+    into left leg front step
 right leg front step
+    into right leg front step
 left leg front step through
+    into left leg front step through
 right leg front step through
+    into right leg front step through
 left leg front kickthrough
+    to left leg front kickthrough
     jump to left leg front kickthrough
+    levitate to left leg front kickthrough
 right leg front kickthrough
+    to right leg front kickthrough
     jump to right leg front kickthrough
+    levitate to right leg front kickthrough
 left leg scorpion switch
     slide to left leg scorpion switch
+    pop back to left leg scorpion switch
 continue the switch (precursor: left leg scorpion reach)
 right leg scorpion switch
     slide to right leg scorpion switch
+    pop back to right leg scorpion switch
 continue the switch (precursor: right leg scorpion reach)
 left leg full scorpion
     into left leg full scorpion
 right leg full scorpion
     into right leg full scorpion
-LEVEL 2
+
+>>LEVEL 2<<
 left leg underswitch to ape
     into left leg underswitch to ape
 right leg underswitch to ape
@@ -92,21 +116,33 @@ left leg reaching underswitch
 right leg reaching underswitch
 left leg scorpion sweep
     slide to left leg scorpion sweep
+    into left leg scorpion sweep
+    pop back to left leg scorpion sweep
 right leg scorpion sweep
     slide to right leg scorpion sweep
+    into right leg scorpion sweep
+    pop back to right leg scorpion sweep
 left side crocodile roll
+    into left side crocodile roll
+pop back to left side crocodile roll
 pop switch (precursor: left side crocodile roll)
 open the gap (precursor: left side crocodile roll)
 right side crocodile roll
+    into right side crocodile roll
+pop back to right side crocodile roll
 pop switch (precursor: right side crocodile roll)
 open the gap (precursor: right side crocodile roll)
 left leg roll through
+    into left leg roll through
 close the gap (precursor: left leg roll through)
 right leg roll through
+    into right leg roll through
 close the gap (precursor: right leg roll through)
 left side pop out
-    left side crocodile roll to pop out
+    into left side pop out
+    to pop out (precursor: left side crocodile roll)
 right side pop out
-    right side crocodile roll to pop out
+    into right side pop out
+    to pop out (precursor: right side crocodile roll)
 
 */ 
